2670ad0bda24976b7df4d16d275bf13d179c68d1
M-SK97 <legendary512@gmail.com>
2020-07-09 02:09:12 +0300

    .

--- Nueral Network/main.py
+++ Nueral Network/main.py
@@ -4,14 +4,14 @@ import NueralNets as nn
 #data = np.loadtxt('data', usecols=range(400), delimiter=',')
 #np.save('data',data)
 
-X = np.load('data.npy') # loads input data matrix X
+X = np.load('Nueral Network/data.npy') # loads input data matrix X
 X = np.mat(X) # Convert into numpy matrix
 
-y = np.load('labels.npy') # loads the corresponded labels for X
+y = np.load('Nueral Network/labels.npy') # loads the corresponded labels for X
 y = np.mat(y).transpose() # convert into column vector
 
-Theta1 = np.loadtxt('Theta1', usecols=range(401), delimiter=',')
-Theta2 = np.loadtxt('Theta2', usecols=range(26), delimiter=',')
+Theta1 = np.loadtxt('Nueral Network/Theta1', usecols=range(401), delimiter=',')
+Theta2 = np.loadtxt('Nueral Network/Theta2', usecols=range(26), delimiter=',')
 Theta = [Theta1,Theta2]
 
 print(nn.cost(X,y,Theta,1,10))

1279873e4792e2a0c7f8b28c1e6f1f216aaf5be0
M-SK97 <legendary512@gmail.com>
2020-07-10 05:35:57 +0300

    .

--- Neural Network/main.py
+++ Neural Network/main.py
@@ -4,16 +4,16 @@ from NeuralNets import NeuralNetworkModel
 #data = np.loadtxt('data', usecols=range(400), delimiter=',')
 #np.save('data',data)
 
-X = np.load('data.npy') # loads input data matrix X
+X = np.load('Input Data/Examples/data.npy') # loads input data matrix X
 X = np.mat(X) # Convert into numpy matrix
 
-y = np.load('labels.npy') # loads the corresponded labels for X
+y = np.load('Input Data/Labels/labels.npy') # loads the corresponded labels for X
 y = np.mat(y).transpose() # convert into column vector
 
 #Theta1 = np.loadtxt('Theta1', usecols=range(401), delimiter=',')
 #Theta2 = np.loadtxt('Theta2', usecols=range(26), delimiter=',')
-Theta = [np.load('Theta1.npy'),np.load('Theta2.npy')]
+Theta = [np.load('Input Data/Test Weights/Theta1.npy'),np.load('Input Data/Test Weights/Theta2.npy')]
 
 NN = NeuralNetworkModel(3,X,y,Theta,1,10) # Creating Neural Net Model
 
-print()
+print(NN.backPropagation())

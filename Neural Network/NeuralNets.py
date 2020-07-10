21a5bc9fe1b966f7c7ce7320e5656fa863e55b3b
M-SK97 <legendary512@gmail.com>
2020-07-10 04:03:50 +0300

    Implemented backpropagation

--- Neural Network/NeuralNets.py
+++ Neural Network/NeuralNets.py
@@ -2,25 +2,30 @@ import numpy as np
 
 class NeuralNetworkModel:
 
-    a = 0
-    z = 0
-    m = 0
-    n = 0
-    def __init__(self,a,X,y):
+    def __init__(self,a,X,y,Theta,Lambda,K):
         self.m = y.shape[0] # Number of examples
         self.n = X.shape[1] # Number of features
-        self.a = np.zeros((a,1))
-        self.z = a
+        self.a = np.zeros((a,1)) # Neurons
+        self.z = a # Function of line
+        self.Theta = Theta # Weights matrices
+        self.K = K # Numver of labels
+        self.Lambda = Lambda # Regularization factor
+        self.X = X
+        self.y = y
 
 
     def sigmoid(self,Z):
         Z = np.mat(Z)
         return 1/(1+np.exp(-Z))
 
+    def sigmoidGradient(self,Z):
+        Z = np.mat(Z)
+        return np.multiply(self.sigmoid(Z),(1-self.sigmoid(Z)))
+
     def cost(self,X,y,Theta,Lambda,K):
         Y = range(1,K+1) == y
 
-        h = self.feedForward(X,y,Theta)
+        h = (self.feedForward(X,y,Theta))[2]
         RegularizationTerm = Lambda/(2*self.m) * (np.sum(np.sum(np.power(Theta[0],2))) + np.sum(np.sum(np.power(Theta[1],2))))
         J = 1/self.m * np.sum( np.sum( np.multiply( -1*Y,np.log(h) ) - np.multiply( (1-Y),np.log(1-h) ) ) ) + RegularizationTerm
 
@@ -36,7 +41,7 @@ class NeuralNetworkModel:
         z3 = a2@(Theta[1].transpose())
         a3 = self.sigmoid(z3)
 
-        return a3
+        return [z2,a2,a3]
 
     def randomWeights(self,r,c):
 
@@ -45,4 +50,16 @@ class NeuralNetworkModel:
 
         return Theta
 
-    #def backPropagation(y,Theta):
+    def backPropagation(self):
+        
+        [z2,a2,a3] = self.feedForward(self.X,self.y,self.Theta)
+        err3 = a3 - self.y
+        err2 = np.multiply(err3*self.Theta[1][:,1:],self.sigmoidGradient(z2))
+        
+        delta1 = self.X.transpose()*err2
+        delta2 = a2.transpose()*err3
+
+        Grad1 = 1/self.m*delta1
+        Grad2 = 1/self.m*delta2
+
+        return [Grad1,Grad2]
